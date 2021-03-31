6ca0df5a6b944bf67b0ed51bd6a56801c9d20f29
pink <123@pink>
2021-03-31 09:57:19 +0800

    初始化服务器,开启跨域

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // 路由中间件
+
+//引入模块
+const express = require('express');
+//开启跨域
+const cors = require('cors');
+
+const app = express();
+//解决跨域
+app.use(cors());
+
+//启动服务器
+app.listen(3000, () => {
+    console.log('服务器3000启动成功....');
+
+})
